25e7f0a698518ba479c10f3cc4b39bfee7b1afd7
sayyadhussain315 <sayyadhussain315@gmail.com>
2026-04-05 23:49:31 +0500

    220260405

--- statefull_widget_prac_counter_app.dart
+++ statefull_widget_prac_counter_app.dart
@@ -3,31 +3,25 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MyHomePage();
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   int _count = 0;
-
   void IncrementCounter() {
     setState(() {
       _count++;
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
