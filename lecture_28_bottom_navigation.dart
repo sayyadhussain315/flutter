4d6a9e0287bdfe313b717fdd6605d03c5b3bb28a
sayyadhussain315 <sayyadhussain315@gmail.com>
2026-05-09 22:40:24 +0500

    20260509

---
+++ lecture_28_bottom_navigation.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  int index = 0;
+   List<String> items = ["Home", "Search", "Settings"];
+   List<Widget> myWidgets = [
+     Text("This is home"),
+     Text("This is Search"),
+     Text("This is Settings"),
+
+
+   ];
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text(items[index],
+        style: TextStyle(fontSize: 30,
+        color: Colors.white,
+        ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(child: Container(
+          child: myWidgets[index],
+        decoration: BoxDecoration(
+          
+        ),
+      )
+
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value)
+          {
+            setState(() {
+              index = value;
+            });
+            print(index);
+          },
+          items: [
+            BottomNavigationBarItem(
+              label: "Home",
+                icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: "Search",
+              icon: Icon(Icons.search),
+            ),
+            BottomNavigationBarItem(
+              label: "Settings",
+              icon: Icon(Icons.settings),
+            )
+      ]
+      ),
+    );
+  }
+}
