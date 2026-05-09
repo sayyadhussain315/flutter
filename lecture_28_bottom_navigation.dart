1550dd17aeeffd914e3981faca5574d8b4b929e5
sayyadhussain315 <sayyadhussain315@gmail.com>
2026-05-09 22:42:46 +0500

    20260509

--- lecture_28_bottom_navigation.dart
+++ lecture_28_bottom_navigation.dart
@@ -32,7 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Center(child: Container(
           child: myWidgets[index],
         decoration: BoxDecoration(
-          
+
         ),
       )
 
