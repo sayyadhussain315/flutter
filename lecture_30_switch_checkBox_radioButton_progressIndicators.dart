12fd721492cd8858e8a0fb301b7866c5dba4a987
sayyadhussain315 <sayyadhussain315@gmail.com>
2026-05-10 23:50:57 +0500

    20260510

--- lecture_30_switch_checkBox_radioButton_progressIndicators.dart
+++ lecture_30_switch_checkBox_radioButton_progressIndicators.dart
@@ -7,6 +7,7 @@ class HomeScreen extends StatefulWidget {
 }
 class _HomeScreenState extends State<HomeScreen> {
   bool iscon = false;
+  String? groupValue = 'one';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,37 +20,76 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Center(
         child: Column(
-          spacing: 55,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Switch( value: iscon,
-              onChanged: (value) {
-               setState(() {
-                iscon = value;
-              });
-              },
+           //  Switch( value: iscon,
+           //    onChanged: (value) {
+           //     setState(() {
+           //      iscon = value;
+           //    });
+           //    },
+           //  ),
+           //  SwitchListTile(
+           //  value: iscon,
+           //  onChanged: (value) {
+           //    setState(() {
+           //      iscon = value;
+           //    });
+           //  },
+           //  title: Text("Switch List Tile: "),
+           //  subtitle: Text("Switch List Tile Sub Title: "),
+           //  ),
+           //
+           //
+           //  Checkbox(value: iscon,
+           //  onChanged:(value)
+           //  {
+           //    setState(() {
+           //      iscon = value!;
+           //    });
+           //  },
+           //  ),
+           //
+           //  CheckboxListTile(value: iscon,
+           //    onChanged:(value)
+           //    {
+           //      setState(() {
+           //        iscon = value!;
+           //      });
+           //    },
+           //
+           //    title: Text("Check box List Tile"),
+           //  ),
+           // RadioMenuButton(value: 'one',
+           //     groupValue: groupValue,
+           //     onChanged: (value)
+           //     {
+           //       setState(() {
+           //         groupValue = value;
+           //         print(groupValue);
+           //       });
+           //     },
+           //     child: Text("One")
+           // ),
+           //  RadioMenuButton(value: 'two',
+           //      groupValue: groupValue,
+           //      onChanged: (value)
+           //      {
+           //        setState(() {
+           //          groupValue = value;
+           //          print(groupValue);
+           //        });
+           //      },
+           //      child: Text("Two")
+           //  ),
+            CircularProgressIndicator(
+              value: 0.7,
+
             ),
-            SwitchListTile(
-            value: iscon,
-            onChanged: (value) {
-              setState(() {
-                iscon = value;
-              });
-            },
-            title: Text("Switch List Tile: "),
-            subtitle: Text("Switch List Tile Sub Title: "),
-
-
-            ),
-
-
-            Checkbox(value: iscon,
-            onChanged:(value)
-            {
-              setState(() {
-                iscon = value!;
-              });
-            },
-            ),
+            SizedBox(height: 10,),
+            LinearProgressIndicator(
+              value: 0.7,
+            )
           ]
         ),
       ),
